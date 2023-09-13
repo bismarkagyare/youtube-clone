236ba9c1c0297fbbbcad419e9a49aefa204d3301
Bismark Agyare <bismarkagyare54@gmail.com>
2023-09-13 21:14:14 +0000

    feat: Integrate Header component into the main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Header from './components/Header/Header';
+import './App.css';
+
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
