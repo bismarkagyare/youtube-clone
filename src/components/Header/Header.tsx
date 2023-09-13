c9fa4306c480e2f288ac805a49c38fb28e556898
Bismark Agyare <bismarkagyare54@gmail.com>
2023-09-13 21:13:06 +0000

    feat: Add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import MicIcon from '@mui/icons-material/Mic';
+import { Avatar } from '@mui/material';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <MenuOutlinedIcon style={{ color: 'white', fontSize: 35 }} />
+        <img
+          className="header-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/YouTube_light_logo_%282017%29.svg/2560px-YouTube_light_logo_%282017%29.svg.png"
+        ></img>
+      </div>
+      <div className="header-input">
+        <div className="header-middle">
+          <input className="searchInput" placeholder="Search" type="text" />
+          <SearchIcon
+            className="header-searchBtn"
+            style={{ color: 'white', fontSize: 30 }}
+          />
+        </div>
+        <MicIcon className="header-icons" style={{ fontSize: 35 }} />
+      </div>
+      <div className="header-right">
+        <VideoCallOutlinedIcon
+          className="header-icons"
+          style={{ fontSize: 35 }}
+        />
+        <NotificationsNoneOutlinedIcon
+          className="header-icons"
+          style={{ fontSize: 35 }}
+        />
+        <Avatar>B</Avatar>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
