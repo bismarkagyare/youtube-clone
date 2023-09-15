060495dcfab05517f7d56e59574e7b4323ac5c30
Bismark Agyare <bismarkagyare54@gmail.com>
2023-09-15 16:29:24 +0000

    feat: Toggle sidebar functionality: Add state and handler for showing/hiding the sidebar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
@@ -16,9 +16,6 @@ import {
   WatchLaterOutlined,
   ThumbUpAltOutlined,
   ExpandMoreOutlined,
-  YouTube,
-  Theaters,
-  SportsEsports,
   SettingsOutlined,
   FlagOutlined,
   HelpOutlineOutlined,
@@ -28,12 +25,19 @@ import SidebarRow from '../Sidebar/SidebarRow';
 import SubscriptionRow from '../Sidebar/SubscriptionRow';
 import '../../Sidebar.css';
 
-const Header = () => {
+const Header: React.FC = () => {
+  const [showSidebar, setshowSidebar] = useState(true);
+  const toggleSidebar = () => {
+    setshowSidebar(!showSidebar);
+  };
+
   return (
     <div className="parent-header">
       <div className="header">
         <div className="header-left">
-          <MenuOutlinedIcon style={{ color: 'white', fontSize: 35 }} />
+          <div onClick={toggleSidebar} className="menu-icon">
+            <MenuOutlinedIcon style={{ color: 'white', fontSize: 35 }} />
+          </div>
           <img
             className="header-logo"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/YouTube_light_logo_%282017%29.svg/2560px-YouTube_light_logo_%282017%29.svg.png"
@@ -61,106 +65,127 @@ const Header = () => {
           <Avatar>B</Avatar>
         </div>
       </div>
-      <div className="sidebar">
-        <SidebarRow icon={React.createElement(HomeIcon)} title="Home" />
-        <SidebarRow
-          icon={React.createElement(ExploreOutlinedIcon)}
-          title="Explore"
-        />
-        <SidebarRow
-          icon={React.createElement(SubscriptionsOutlinedIcon)}
-          title="Subscriptions"
-        />
-        <hr></hr>
-        <SidebarRow
-          icon={React.createElement(VideoLibraryOutlinedIcon)}
-          title="Library"
-        />
-        <SidebarRow icon={React.createElement(History)} title="History" />
-        <SidebarRow
-          icon={React.createElement(OndemandVideo)}
-          title="Your videos"
-        />
-        <SidebarRow
-          icon={React.createElement(WatchLaterOutlined)}
-          title="Watch later"
-        />
-        <SidebarRow
-          icon={React.createElement(ThumbUpAltOutlined)}
-          title="Liked videos"
-        />
-        <SidebarRow
-          icon={React.createElement(ExpandMoreOutlined)}
-          title="Show more"
-        />
-        <hr></hr>
-        <h4 className="sidebar-heading">Subscriptions</h4>
-        <SubscriptionRow
-          img="https://yt3.ggpht.com/UlKrbeZ4Xz79DUbEbF3FvC0FQ4A_cvpIIzhJQ_wigP8CL_Xf_WF-ryYrrtGpqpD9WzAplsUz=s88-c-k-c0x00ffffff-no-rj"
-          name="Hyperplexed"
-        />
-        <SubscriptionRow
-          img="https://yt3.ggpht.com/BWW7dv17Ls1Voen8QWO3rE37bFRnjt-lnX3P0MhLfY2Ea5SRMzfowPHhReNoU_GYNoT1Zbc_Ow=s88-c-k-c0x00ffffff-no-rj"
-          name="Undisputed"
-        />
-        <SubscriptionRow
-          img="https://yt3.ggpht.com/70r5TkYTLC0cpKLAiQEvcWLeIHB8yxoiog0nQIK9MmnZHqkICy0YA-jAaqfT2ChOBwehskjf5g=s88-c-k-c0x00ffffff-no-rj"
-          name="TEDx"
-        />
-        <SubscriptionRow
-          img="https://yt3.ggpht.com/ytc/AOPolaQ6GOG-h4l3S1N5fGuJJ5FLdZvSQhs_HIKlb3TSPQ=s88-c-k-c0x00ffffff-no-rj"
-          name="REACT"
-        />
-        <SubscriptionRow
-          img="https://yt3.ggpht.com/ytc/AOPolaTO-q353uQfU-doXj7jgstXm3RbyrTqWmOAKNH34Q=s88-c-k-c0x00ffffff-no-rj"
-          name="Be Inspired"
-        />
-        <SidebarRow
-          icon={React.createElement(ExpandMoreOutlined)}
-          title="Show more"
-        />
-        <hr></hr>
-        <h4 className="sidebar-heading">More from Youtube</h4>
-        <SidebarRow
-          icon="https://www.svgrepo.com/show/343535/youtube-music-song-multimedia-audio.svg"
-          title="YouTube Studio"
-          style={{ width: '30px', height: '30px', fill: 'red' }}
-        />
-        <SidebarRow
-          icon="https://www.svgrepo.com/show/452138/youtube.svg"
-          title="YouTube Kids"
-          style={{ width: '30px', height: '30px', fill: 'red' }}
-        />
-        <hr></hr>
-        <SidebarRow
-          icon={React.createElement(SettingsOutlined)}
-          title="Settings"
-        />
-        <SidebarRow
-          icon={React.createElement(FlagOutlined)}
-          title="Report History"
-        />
-        <SidebarRow
-          icon={React.createElement(HelpOutlineOutlined)}
-          title="Help"
-        />
-        <SidebarRow
-          icon={React.createElement(FeedbackOutlined)}
-          title="Send Feedback"
-        />
-        <hr></hr>
-        <div className="footer">
-          <p>About &nbsp; Press &nbsp; Copyright</p>
-          <p>Contact us &nbsp; Creators</p>
-          <p>Advertise &nbsp; Developers</p>
-          <br />
-          <p>Terms &nbsp; Privacy &nbsp; Policy & Safety</p>
-          <p>How YouTube works</p>
-          <p>Test new features</p>
-          <br />
-          <p className="llc">&#169; 2023 Google LLC</p>
+      {showSidebar ? (
+        <div className="sidebar">
+          <SidebarRow icon={React.createElement(HomeIcon)} title="Home" />
+          <SidebarRow
+            icon={React.createElement(ExploreOutlinedIcon)}
+            title="Explore"
+          />
+          <SidebarRow
+            icon={React.createElement(SubscriptionsOutlinedIcon)}
+            title="Subscriptions"
+          />
+          <hr></hr>
+          <SidebarRow
+            icon={React.createElement(VideoLibraryOutlinedIcon)}
+            title="Library"
+          />
+          <SidebarRow icon={React.createElement(History)} title="History" />
+          <SidebarRow
+            icon={React.createElement(OndemandVideo)}
+            title="Your videos"
+          />
+          <SidebarRow
+            icon={React.createElement(WatchLaterOutlined)}
+            title="Watch later"
+          />
+          <SidebarRow
+            icon={React.createElement(ThumbUpAltOutlined)}
+            title="Liked videos"
+          />
+          <SidebarRow
+            icon={React.createElement(ExpandMoreOutlined)}
+            title="Show more"
+          />
+          <hr></hr>
+          <h4 className="sidebar-heading">Subscriptions</h4>
+          <SubscriptionRow
+            img="https://yt3.ggpht.com/UlKrbeZ4Xz79DUbEbF3FvC0FQ4A_cvpIIzhJQ_wigP8CL_Xf_WF-ryYrrtGpqpD9WzAplsUz=s88-c-k-c0x00ffffff-no-rj"
+            name="Hyperplexed"
+          />
+          <SubscriptionRow
+            img="https://yt3.ggpht.com/BWW7dv17Ls1Voen8QWO3rE37bFRnjt-lnX3P0MhLfY2Ea5SRMzfowPHhReNoU_GYNoT1Zbc_Ow=s88-c-k-c0x00ffffff-no-rj"
+            name="Undisputed"
+          />
+          <SubscriptionRow
+            img="https://yt3.ggpht.com/70r5TkYTLC0cpKLAiQEvcWLeIHB8yxoiog0nQIK9MmnZHqkICy0YA-jAaqfT2ChOBwehskjf5g=s88-c-k-c0x00ffffff-no-rj"
+            name="TEDx"
+          />
+          <SubscriptionRow
+            img="https://yt3.ggpht.com/ytc/AOPolaQ6GOG-h4l3S1N5fGuJJ5FLdZvSQhs_HIKlb3TSPQ=s88-c-k-c0x00ffffff-no-rj"
+            name="REACT"
+          />
+          <SubscriptionRow
+            img="https://yt3.ggpht.com/ytc/AOPolaTO-q353uQfU-doXj7jgstXm3RbyrTqWmOAKNH34Q=s88-c-k-c0x00ffffff-no-rj"
+            name="Be Inspired"
+          />
+          <SidebarRow
+            icon={React.createElement(ExpandMoreOutlined)}
+            title="Show more"
+          />
+          <hr></hr>
+          <h4 className="sidebar-heading">More from Youtube</h4>
+          <SidebarRow
+            icon="https://www.svgrepo.com/show/343535/youtube-music-song-multimedia-audio.svg"
+            title="YouTube Studio"
+            style={{ width: '30px', height: '30px', fill: 'red' }}
+          />
+          <SidebarRow
+            icon="https://www.svgrepo.com/show/452138/youtube.svg"
+            title="YouTube Kids"
+            style={{ width: '30px', height: '30px', fill: 'red' }}
+          />
+          <hr></hr>
+          <SidebarRow
+            icon={React.createElement(SettingsOutlined)}
+            title="Settings"
+          />
+          <SidebarRow
+            icon={React.createElement(FlagOutlined)}
+            title="Report History"
+          />
+          <SidebarRow
+            icon={React.createElement(HelpOutlineOutlined)}
+            title="Help"
+          />
+          <SidebarRow
+            icon={React.createElement(FeedbackOutlined)}
+            title="Send Feedback"
+          />
+          <hr></hr>
+          <div className="footer">
+            <p>About &nbsp; Press &nbsp; Copyright</p>
+            <p>Contact us &nbsp; Creators</p>
+            <p>Advertise &nbsp; Developers</p>
+            <br />
+            <p>Terms &nbsp; Privacy &nbsp; Policy & Safety</p>
+            <p>How YouTube works</p>
+            <p>Test new features</p>
+            <br />
+            <p className="llc">&#169; 2023 Google LLC</p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="mini-sidebar">
+          <div className="mini-div">
+            <HomeIcon style={{ fontSize: 35 }} />
+            <p>Home</p>
+          </div>
+          <div className="mini-div">
+            <ExploreOutlinedIcon style={{ fontSize: 35 }} />
+            <p>Explore</p>
+          </div>
+          <div className="mini-div">
+            <SubscriptionsOutlinedIcon style={{ fontSize: 35 }} />
+            <p>Subscription</p>
+          </div>
+          <div className="mini-div">
+            <VideoLibraryOutlinedIcon style={{ fontSize: 35 }} />
+            <p>Library</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
